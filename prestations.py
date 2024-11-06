7a91a0a0a1c1ca137cdebcd134574520a6878a08
Swallowedin <charles.thomasse@gmail.com>
2024-11-06 17:13:10 +0100

    Update prestations.py

--- prestations.py
+++ prestations.py
@@ -8,11 +8,6 @@ def get_prestations():
                     "tarif": 800,
                     "definition": "Élaboration des termes et conditions régissant les relations entre une entreprise et ses clients."
                 },
-                "redaction_contrat_mise_disposition_prestations_services_associes": {
-                    "label": "Rédaction de contrat de mise à disposition avec prestations de services associées",
-                    "tarif": 1500,
-                    "definition": "Préparation d'un contrat détaillant la mise à disposition de ressources et les services associés."
-                },
                 "consultation_initiale": {
                     "label": "Consultation initiale",
                     "tarif": 200,
@@ -23,11 +18,6 @@ def get_prestations():
                     "tarif": 800,
                     "definition": "Analyse approfondie d'une situation juridique spécifique et conseil sur les implications réglementaires."
                 },
-                "creation_entreprise": {
-                    "label": "Création d'entreprise",
-                    "tarif": 800,
-                    "definition": "Accompagnement juridique complet pour la création d'une nouvelle entreprise."
-                },
                 "redaction_contrat_simple": {
                     "label": "Rédaction de contrat simple",
                     "tarif": 800,
@@ -37,21 +27,6 @@ def get_prestations():
                     "label": "Rédaction de contrat complexe",
                     "tarif": 2000,
                     "definition": "Préparation d'un contrat détaillé couvrant des situations juridiques complexes ou multiples."
-                },
-                "procedure_divorce_amiable": {
-                    "label": "Procédure de divorce amiable",
-                    "tarif": 3750,
-                    "definition": "Gestion juridique d'un divorce par consentement mutuel, incluant la rédaction de la convention."
-                },
-                "redaction_statuts_societe": {
-                    "label": "Rédaction des statuts de société",
-                    "tarif": 600,
-                    "definition": "Élaboration du document juridique fondamental définissant la structure et le fonctionnement d'une société."
-                },
-                "depot_marque": {
-                    "label": "Dépôt de marque",
-                    "tarif": 1000,
-                    "definition": "Procédure juridique pour protéger une marque commerciale auprès des autorités compétentes."
                 }
             }
         },
@@ -61,107 +36,107 @@ def get_prestations():
                 "redaction_bail_commercial": {
                     "label": "Rédaction de bail commercial",
                     "tarif": 1500,
-                    "definition": "Préparation d'un contrat de location pour un bien immobilier à usage commercial."
+                    "definition": "Préparation d'un contrat de location pour un bien immobilier à usage commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_bail_commercial_en_etat_futur_achevement_BEFA": {
                     "label": "Rédaction de bail commercial en état futur d'achèvement (BEFA)",
                     "tarif": 2500,
-                    "definition": "Élaboration d'un bail pour un local commercial encore en construction ou en rénovation."
+                    "definition": "Élaboration d'un bail pour un local commercial encore en construction ou en rénovation. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_bail_commercial_derogatoire": {
                     "label": "Rédaction de bail commercial dérogatoire",
                     "tarif": 1000,
-                    "definition": "Préparation d'un bail commercial de courte durée, dérogeant au statut des baux commerciaux."
+                    "definition": "Préparation d'un bail commercial de courte durée, dérogeant au statut des baux commerciaux. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_bail_sous_location": {
                     "label": "Rédaction de bail de sous-location",
                     "tarif": 1000,
-                    "definition": "Élaboration d'un contrat permettant à un locataire de louer tout ou partie du bien à un tiers."
+                    "definition": "Élaboration d'un contrat permettant à un locataire de louer tout ou partie du bien à un tiers. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_bail_professionnel": {
                     "label": "Rédaction de bail professionnel",
                     "tarif": 800,
-                    "definition": "Préparation d'un contrat de location pour un local destiné à l'exercice d'une profession libérale."
+                    "definition": "Préparation d'un contrat de location pour un local destiné à l'exercice d'une profession libérale. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_avenant_bail_commercial": {
                     "label": "Rédaction d'avenant à bail commercial",
                     "tarif": 500,
-                    "definition": "Élaboration d'un document modifiant les termes d'un bail commercial existant."
+                    "definition": "Élaboration d'un document modifiant les termes d'un bail commercial existant. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_demande_revision_loyer": {
                     "label": "Rédaction de demande de révision de loyer",
                     "tarif": 500,
-                    "definition": "Préparation d'une requête formelle pour ajuster le montant du loyer d'un bail commercial."
+                    "definition": "Préparation d'une requête formelle pour ajuster le montant du loyer d'un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_demande_despecialisation": {
                     "label": "Rédaction de demande de déspécialisation",
                     "tarif": 400,
-                    "definition": "Élaboration d'une demande pour modifier l'activité autorisée ou la destination dans un bail commercial."
+                    "definition": "Élaboration d'une demande pour modifier l'activité autorisée ou la destination dans un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "procedure_resiliation_bail_commercial": {
                     "label": "Procédure de résiliation de bail commercial",
                     "tarif": 1500,
-                    "definition": "Gestion juridique de la fin anticipée d'un bail commercial."
+                    "definition": "Gestion juridique de la fin anticipée d'un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_commandement_clause_resolutoire": {
                     "label": "Rédaction de commandement visant la clause résolutoire",
                     "tarif": 400,
-                    "definition": "Préparation d'un acte formel mettant en œuvre la clause de résiliation automatique du bail."
+                    "definition": "Préparation d'un acte formel mettant en œuvre la clause de résiliation automatique du bail. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "mise_en_demeure_conflit_bail_commercial": {
                     "label": "Mise en demeure pour conflit de bail commercial",
                     "tarif": 400,
-                    "definition": "Rédaction d'un avertissement formel dans le cadre d'un litige lié à un bail commercial."
+                    "definition": "Rédaction d'un avertissement formel dans le cadre d'un litige lié à un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "procedure_recouvrement_impayes": {
                     "label": "Procédure de recouvrement d'impayés",
                     "tarif": 500,
-                    "definition": "Gestion juridique pour récupérer des loyers ou charges impayés dans le cadre d'un bail commercial."
+                    "definition": "Gestion juridique pour récupérer des loyers ou charges impayés dans le cadre d'un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_conge": {
                     "label": "Rédaction de congé",
                     "tarif": 400,
-                    "definition": "Préparation de l'acte mettant fin à un bail commercial à son terme."
+                    "definition": "Préparation de l'acte mettant fin à un bail commercial à son terme. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_demande_renouvellement": {
                     "label": "Rédaction de demande de renouvellement",
                     "tarif": 400,
-                    "definition": "Élaboration d'une requête formelle pour prolonger un bail commercial arrivant à échéance."
+                    "definition": "Élaboration d'une requête formelle pour prolonger un bail commercial arrivant à échéance. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_memoire_fixation_loyer_bail_renouvele": {
                     "label": "Rédaction de mémoire pour la fixation du loyer de bail renouvelé",
                     "tarif": 1000,
-                    "definition": "Préparation d'un document argumenté pour déterminer le nouveau loyer lors du renouvellement d'un bail."
+                    "definition": "Préparation d'un document argumenté pour déterminer le nouveau loyer lors du renouvellement d'un bail. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "procedure_juge_loyers_commerciaux": {
                     "label": "Procédure devant le juge des loyers commerciaux",
                     "tarif": 3000,
-                    "definition": "Représentation juridique dans un litige concernant le loyer d'un bail commercial."
+                    "definition": "Représentation juridique dans un litige concernant le loyer d'un bail commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "purge_droit_preemption": {
                     "label": "Purge du droit de préemption",
                     "tarif": 500,
-                    "definition": "Procédure visant à s'assurer qu'aucun tiers n'a de droit prioritaire d'achat sur un bien commercial."
+                    "definition": "Procédure visant à s'assurer qu'aucun tiers n'a de droit prioritaire d'achat sur un bien commercial. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "redaction_cession_droit_bail": {
                     "label": "Rédaction de cession de droit au bail",
                     "tarif": 1500,
-                    "definition": "Élaboration du contrat transférant les droits et obligations d'un bail commercial à un nouveau locataire."
+                    "definition": "Élaboration du contrat transférant les droits et obligations d'un bail commercial à un nouveau locataire. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "procedure_bail_commercial_tribunal_judiciaire_fond": {
                     "label": "Procédure de bail commercial devant le tribunal judiciaire (fond)",
                     "tarif": 2000,
-                    "definition": "Représentation juridique dans un litige complexe lié à un bail commercial devant le tribunal."
+                    "definition": "Représentation juridique dans un litige complexe lié à un bail commercial devant le tribunal. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "procedure_fixation_indemnite_eviction": {
                     "label": "Procédure de fixation d'indemnité d'éviction",
                     "tarif": 4000,
-                    "definition": "Gestion juridique pour déterminer la compensation due au locataire en cas de non-renouvellement du bail."
+                    "definition": "Gestion juridique pour déterminer la compensation due au locataire en cas de non-renouvellement du bail. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat"
                 },
                 "consultation_juridique_et_reglementaire": {
                     "label": "Consultation juridique et réglementaire",
                     "tarif": 800,
-                    "definition": "Analyse approfondie d'une situation juridique spécifique et conseil sur les implications réglementaires." 
+                    "definition": "Analyse approfondie d'une situation juridique spécifique et conseil sur les implications réglementaires. Valable pour les professionnels, les personnes morales ou les particuliers se lançant dans l'entrepreneuriat" 
                 }    
             }
         },
@@ -315,12 +290,12 @@ def get_prestations():
             "prestations": {
                 "procedure_divorce_contentieux": {
                     "label": "Procédure de divorce contentieux",
-                    "tarif": 4500,
+                    "tarif": 2000,
                     "definition": "Gestion juridique d'une procédure de divorce non consensuelle."
                 },
                 "procedure_divorce_amiable": {
                     "label": "Procédure de divorce amiable",
-                    "tarif": 3000,
+                    "tarif": 1500,
                     "definition": "Accompagnement juridique pour un divorce par consentement mutuel."
                 },
                 "pension_alimentaire": {
@@ -856,12 +831,12 @@ def get_prestations():
                 "droit_de_la_propriété": {
                     "label": "Droit de la propriété",
                     "tarif": 2200,
-                    "definition": "Assistance juridique en matière de litiges ayant pour origine le non respect de la propriété privée."
+                    "definition": "Assistance juridique en matière de litiges ayant pour origine le non respect de la propriété privée. Concerne en général pour les particuliers"
                 },
                 "droit_copropriété": {
                     "label": "Droit de la copropriété",
                     "tarif": 2000,
-                    "definition": "Gestion des litiges et conseil en droit de la copropriété."
+                    "definition": "Gestion des litiges et conseil en droit de la copropriété. Concerne en général pour les particuliers"
                 },
                 "création_société_civile_immobilière": {
                     "label": "Création de statuts de société SCI",
@@ -951,7 +926,7 @@ def get_prestations():
                 "externalisation_partielle": {  # Nouvelle option plus flexible
                     "label": "Externalisation juridique partielle",
                     "tarif": 1200,
-                    "definition": "Accompagnement juridique sur des domaines spécifiques de l'entreprise, en complément des ressources internes existantes."
+                    "definition": "Accompagnement juridique sur des domaines spécifiques de l'entreprise, en complément des ressources internes existantes. Il s'agit d'externaliser une partie de l'équipe juridique d'une personne morale ou d'une société dans un doomaine précis : immobilier, contrats, contentieux..."
                 },
                 "conseil_juridique_ponctuel": {
                     "label": "Conseil juridique ponctuel",
